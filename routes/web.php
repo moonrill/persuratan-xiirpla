a09447ab8a059d6f5f4e71c7908303bb0a54181d
arilramadani24 <arilramadani245@gmail.com>
2023-10-04 07:20:28 +0700

    feat: redirect to dashboard if access base url

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect('/dashboard');
+});
+
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
